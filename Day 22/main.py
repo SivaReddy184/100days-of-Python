9b9f1e8fd0a7388fbcb0f3cdd711047dc050e63d
Siva Reddy <sivareddymanne18@gmail.com>
2022-11-28 21:23:06 +0530

    day 22 files added

---
+++ Day 22/main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+is_game_on = True
+while is_game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    # detecting collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # bounce the ball
+        ball.bounce_y()
+    # detecting collision with paddle
+    if ball.distance(r_paddle) < 60 and ball.xcor() > 320 or ball.distance(l_paddle) < 60 and ball.xcor() < -320:
+        ball.bounce_x()
+    # detecting paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
